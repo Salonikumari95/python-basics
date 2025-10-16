d1301e6c8e5c107f3f1c6cdd55c74ece9a7c0d13
Saloni Kumari <saloni1234.sk@gmail.com>
2025-10-16 21:01:41 +0530

    practice questions

--- practice.py
+++ practice.py
@@ -4,7 +4,7 @@
 
 # 3. Check the type of variable assigned using input () function.
 
-# 4. Use comparison operator to find out whether a given 
+# 4. Use comparison operator to find out whether a given
 # variable a is greater than 'b' or not. Take a = 34 and b = 80
 
 # 5. Write a python program to find an average of two
@@ -43,3 +43,21 @@ print(f"Average of two no is : {(a+b)/2}")
 # 6.
 v = int(input("Enter any number:"))  
 print(f"Square of no is : {v**2}")
+
+
+# 1. Write a python program to display a user entered name followed by Good
+# Afternoon using input () function.
+
+# 2. Write a program to fill in a letter template given below with
+# name and date.
+# letter =
+# Dear <Name>,
+# You are selected!
+# </Date|>
+# 3. Write a program to detect double space in a string.
+
+# 4. Replace the double space from problem 3 with single spaces.
+
+# 5. Write a program to format the following letter using 
+# escape sequence characters.
+# Letter = "Dear Saloni
