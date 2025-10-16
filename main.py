efd94f6615df9cfbe3abe0ef55105a74917a1426
Saloni Kumari <saloni1234.sk@gmail.com>
2025-10-16 20:21:50 +0530

    practice question

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-#Input and Output
+# Input and Output
 name = input("Enter your name")
 print(f"Good morning {name}")
 
-#conditionals  statements
+# conditionals  statements
 a = int(input("Enter 1st no :"))
 b = int(input("Enter 2nd no :"))
 c = int(input("Enter 3rd no :"))
-if(a>b and a>c):
+if (a > b and a > c):
     print("A is greatest no.")
-elif(b>a and b>c):
+elif (b > a and b > c):
     print("B is greatest no.")
 else:
     print("C is greatest no.")        
 
-#Loops
+# Loops
 # *
 # **
 # ***
@@ -22,40 +22,39 @@ else:
 for i in range(5):
     print("*" * (i))
 
-#print 0 to 4    
-i=0
-while(i<5) :
+# print 0 to 4    
+i = 0
+while (i < 5):
     print(i)
-    i +=1   
-    
-    
-#list
-a = ["saloni",8,9,True,5]
+    i += 1   
+     
+# list
+a = ["saloni", 8, 9, True, 5]
 for i in a:
     print(i) 
 
-#tuple
-p = (5,7,2,9,0)
+# tuple
+p = (5, 7, 2, 9, 0)
 for i in p:
     print(i)       
     
 # sets  it does not provide ordered value and cannot access by index no
-q = set() #empty set
-w = {3,7,"saloni",True,9}   
+q = set()  # empty set
+w = {3, 7, "saloni", True, 9}   
 for i in w:
     print(i)
     
-#dictionary
-dict = {} #empty dictionary
-z = {"saloni":5,"rohan":8,"Shubham":6}   
+# dictionary
+dict = {}  # empty dictionary
+z = {"saloni": 5, "rohan": 8, "Shubham": 6}   
 print(z["saloni"])
 
-#file handling reading and writing in file
+# file handling reading and writing in file
     
 with open("saloni.txt", "w") as f:
-        f.write("welcome back")
+    f.write("welcome back")
 with open("saloni.txt", "r") as f:
     print(f.read()) 
 
 
-           
+          
---
+++ practice.py
@@ -0,0 +1,45 @@
+# 1. Write a python program to add two numbers.
+
+# 2. Write a python program to find remainder when a number is divided by z.
+
+# 3. Check the type of variable assigned using input () function.
+
+# 4. Use comparison operator to find out whether a given 
+# variable a is greater than 'b' or not. Take a = 34 and b = 80
+
+# 5. Write a python program to find an average of two
+# numbers entered by the user.
+
+# 6. Write a python program to calculate the square of a 
+# number entered by the user.
+
+# 1.
+a = 10
+b = 15
+print(a+b)
+
+# 2.
+n = int(input("enter any number:"))
+z = 3
+print(n % 3)
+
+# 3
+name = input("enter your name")
+print(type(name))
+
+# 4.
+a = 34
+b = 84
+if (a > b):
+    print("A is greater no")
+else:
+    print("B is greater no")   
+    
+# 5.
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:")) 
+print(f"Average of two no is : {(a+b)/2}")  
+
+# 6.
+v = int(input("Enter any number:"))  
+print(f"Square of no is : {v**2}")
